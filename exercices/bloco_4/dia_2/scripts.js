535b73aac2e94e1ca267fe85ebac1a0f8e5b4244
Kelner Roberto <kelner250@hotmail.com>
2021-06-24 15:23:43 -0300

    adiciona exercicio de Js

---
+++ exercices/bloco_4/dia_2/scripts.js
@@ -0,0 +1,12 @@
+// exercícios
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/*
+for (let i = 0; i < numbers.length; i += 1) {
+    console.log (numbers[i]);
+}
+para imprimir os elementos do array 
+*/
+
+
