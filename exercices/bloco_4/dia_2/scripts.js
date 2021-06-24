42f86d78e44f1cd0138038121747cc5e8728d488
Kelner Roberto <kelner250@hotmail.com>
2021-06-24 15:53:47 -0300

    faz segundo requisito

--- exercices/bloco_4/dia_2/scripts.js
+++ exercices/bloco_4/dia_2/scripts.js
@@ -2,11 +2,21 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-/*
+/* Para imprimir os elementos do array:
+
 for (let i = 0; i < numbers.length; i += 1) {
     console.log (numbers[i]);
 }
-para imprimir os elementos do array 
+
 */
 
+/* Para somar os elementos do array e imprimir a soma deles:
+
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers [index];
+}
+console.log (soma);
+
+*/
 
