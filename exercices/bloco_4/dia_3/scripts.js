c5bef0dd78132c7669bef44881ae2dd09a308517
Kelner Roberto <kelner250@hotmail.com>
2021-06-25 15:08:20 -0300

    adiciona exercícios 1 e 2 do dia 3

---
+++ exercices/bloco_4/dia_3/scripts.js
@@ -0,0 +1,35 @@
+// Exercícios
+
+/* Exercício 1. Faça imprimir um quadrado de asteriscos de lado e tamanho n, sendo n > 1. 
+
+let n = 5;
+let asterisco = '*';
+let outPut = '';
+
+for (let index = 0; index < n; index += 1) {
+    outPut = outPut + asterisco;
+}
+    for (let i = 0; i < n; i += 1) {
+        console.log (outPut);
+}
+
+*/
+
+// -------------------------------------------------------------------------------------------
+
+/* Exercício 2. Imprimir um triângulo retângulo de asteriscos.
+
+let n = 5;
+let asterisco = '*';
+let outPut = '';
+
+for (let index = 0; index < n; index += 1) {
+    outPut += asterisco;
+    console.log (outPut);
+}
+
+*/
+
+//-------------------------------------------------------------------------------------------
+
+/* Exercício 3. 
