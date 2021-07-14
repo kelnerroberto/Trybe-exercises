20ee72ec345268d1ed07e0c34dcce1d361cab520
Kelner Roberto <kelner250@hotmail.com>
2021-07-14 19:42:05 -0300

    exercícios de forms

---
+++ exercices/bloco_6/dia_1/scripts.js
@@ -0,0 +1,39 @@
+
+const stateOptions = document.getElementById('user-state');
+let arrOfStates = ['Acre',
+    'Alagoas',
+    'Amapá',
+    'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins'];
+
+function createStatesOption() {
+    for (let index = 0; index < arrOfStates.length; index += 1) {
+        let selectorOption = document.createElement('option');
+        selectorOption.innerText = arrOfStates[index];
+        selectorOption.value = arrOfStates[index];
+        stateOptions.appendChild(selectorOption);
+    }
+}
+createStatesOption();
