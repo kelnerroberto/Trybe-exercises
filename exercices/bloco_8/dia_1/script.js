ec1cfdd386fe3a2ab54f22533b5320ec739c4589
Kelner Roberto <kelner250@hotmail.com>
2021-07-28 18:52:38 -0300

    exercicios finalizados

--- exercices/bloco_8/dia_1/script.js
+++ exercices/bloco_8/dia_1/script.js
@@ -1,4 +1,4 @@
-/*const wakingUp = () => 'Acordando!!';
+const wakingUp = () => 'Acordando!!';
 
 const breakfast = () => 'Bora tomar café!';
 
@@ -7,8 +7,8 @@ const sleeping = () => 'Partiu dormir!';
 const doingThings = (param) => console.log(param());
 
 doingThings(sleeping);
-*/
-/*
+
+
 const hiredPerson = (name) => ({ nomeCompleto: name, email: `${name.replace(/\s+/g, '_')}@trybe.com`, });
 // Método "string.replace()" retirado de : https://qastack.com.br/programming/5963182/how-to-remove-spaces-from-a-string-using-javascript.
 
@@ -23,7 +23,7 @@ const newEmployees = () => {
 
 
 console.log(newEmployees());
-*/
+
 
 const numeroSorteado = (max) => Math.floor(Math.random() * max) + 1;
 const sorteio = (number, func) => {
@@ -37,3 +37,22 @@ Que pena, tente novamente.`;
 
 console.log(sorteio(3, numeroSorteado(5)));
 
+
+const gabarito = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const kelnerAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const checkAnswers = (rightAnswers, studentAnswers, checkIfIsCorrect) => {
+  return checkIfIsCorrect();
+}
+
+const teste = checkAnswers(gabarito, kelnerAnswers, () => {
+  let counter = 0;
+  for (let index = 0; index < gabarito.length; index += 1){
+    if (gabarito[index] === kelnerAnswers[index]){
+      counter += 1;
+    }
+  }
+  return counter;
+});
+
+console.log(teste);
