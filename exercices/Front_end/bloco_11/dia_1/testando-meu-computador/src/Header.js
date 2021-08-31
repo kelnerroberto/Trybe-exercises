ce8e4018b60663d609c4da73a65296130a190dfa
Kelner Roberto <kelner250@hotmail.com>
2021-08-31 10:55:18 -0300

    exercicios front end

--- exercices/Front_end/bloco_11/dia_1/testando-meu-computador/src/Header.js
+++ exercices/Front_end/bloco_11/dia_1/testando-meu-computador/src/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
   render() {
     return (
       <header className='header'>
-        <a href='https://www.google.com' className='a-header'>Redirecionar</a>
+        <a href='https://www.google.com' target='_blank' className='a-header' rel="noreferrer">Go to GoOgle</a>
         <p className='p-header'>Exerício de react</p>
         <div className='search-box'>
           <input type='text' className='search-input'></input>
